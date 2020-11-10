a57d83a9ff7f837216eada35edbf92bc39a02d24
kenderb <kenderbolivar@gmail.com>
2020-11-10 09:46:00 -0500

    Add Insert Query

--- MySQLExamples/practice.sql
+++ MySQLExamples/practice.sql
@@ -1,10 +1,12 @@
 CREATE DATABASE companyHR;
 USE companyHR;
+
+-- Create tables
+
 CREATE TABLE co_employees (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     em_name VARCHAR(255) NOT NULL,
     gender CHAR(1) NOT NULL,
-    contact_number VARCHAR(255),
     age INT NOT NULL,
     data_created TIMESTAMP NOT NULL DEFAULT NOW()
 );
@@ -23,6 +25,8 @@ CREATE TABLE mentorships (
 
 RENAME TABLE co_employees TO employees;
 
+-- alter tables
+
 ALTER TABLE employees DROP COLUMN age, 
 ADD COLUMN salary FLOAT NOT NULL AFTER contact_number, 
 ADD COLUMN years_in_company INT NOT NULL AFTER salary;
@@ -38,4 +42,44 @@ ALTER TABLE mentorships
 employees(id) ON DELETE CASCADE ON UPDATE CASCADE,
 	DROP INDEX mm_constraint;
 
-DESCRIBE mentorships;
+
+-- show table
+DESCRIBE employees;
+
+-- Insert Values to the tables.
+
+INSERT INTO employees (em_name, gender, contact_number, salary, years_in_company) VALUES
+('James Lee', 'M', '516-514-6568', 3500, 11),
+('Peter Pasternak', 'M', '845-644-7919', 6010, 10),
+('Clara Couto', 'F', '845-641-5236', 3900, 8),
+('Walker Welch', 'M', NULL, 2500, 4),
+('Li Xiao Ting', 'F', '646-218-7733', 5600, 4),
+('Joyce Jones', 'F', '523-172-2191', 8000, 3),
+('Jason Cerrone', 'M', '725-441-7172', 7980, 2),
+('Prudence Phelps', 'F', '546-312-5112', 11000, 2),
+('Larry Zucker', 'M', '817-267-9799', 3500, 1),
+('Serena Parker', 'F', '621-211-7342', 12000, 1);
+
+
+INSERT INTO mentorships (mentor_id, mentee_id, status, project)
+VALUES
+(1, 2, 'Ongoing', 'SQF Limited'),
+(1, 3, 'Past', 'Wayne Fibre'),
+(2, 3, 'Ongoing', 'SQF Limited'),
+(3, 4, 'Ongoing', 'SQF Limited'),
+(6, 5, 'Past', 'Flynn Tech');
+
+INSERT INTO mentorships VALUES
+(1, 2, 'Ongoing', 'SQF Limited'),
+(1, 3, 'Past', 'Wayne Fibre'),
+(2, 3, 'Ongoing', 'SQF Limited'),
+(3, 4, 'Ongoing', 'SQF Limited'),
+(6, 5, 'Past', 'Flynn Tech');
+
+-- update values in the table
+UPDATE employees SET contact_number = '513-514-1729' WHERE id = 1;
+
+
+
+
+
